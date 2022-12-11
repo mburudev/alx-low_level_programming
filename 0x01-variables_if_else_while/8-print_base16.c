6d35e89d1b0cbb1441145e4f51c7cb7ca7bcdff3
mburudev <ianmburu999@gmail.com>
2022-12-11 13:24:30 -0800

    base16 task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+	char alphabet;
+	for (x = '0'; x <= '9'; x++)
+	putchar(x);
+	for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
+	putchar(alphabet);
+	putchar('\n');
+	return (0);
+}
