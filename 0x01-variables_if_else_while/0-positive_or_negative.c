21f5d4dd0492755549115a9a3ca2d99cd7a11145
mburudev <ianmburu999@gmail.com>
2022-12-11 07:55:33 -0800

    correction on characters attempt 3

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,12 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+ 
 int main(void)
 {
 	  int n;
@@ -12,12 +18,12 @@ int main(void)
 	      n = rand() - RAND_MAX / 2;
 	      /* your code goes there */
 
-	          if (n > 0){
-			          printf(" %d is positive",n);
-				      }else if (n < 0){
-					              printf(" %d is negative",n);
-						          }else {
-								          printf(" %d is zero",n);
-									      }
-		  return (0);
+	          if (n > 0)
+			          printf(" %d is positive\n",n);
+		      else if (n < 0)
+			              printf(" %d is negative\n",n);
+		          else 
+				          printf(" %d is zero\n",n);
+			      
+			      return (0);
 }
