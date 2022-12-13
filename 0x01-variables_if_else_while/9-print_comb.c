67793dc4e97935c4a30484aa9f981dd3507cb643
mburudev <ianmburu999@gmail.com>
2022-12-13 05:07:49 -0800

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+/**
+ *
+ * File: 9-print_comb.c
+ *
+ * Auth: Ian Mburu
+ *
+ */
+
+#include <stdio.h>
+
+/**                                 
+ *
+ * main - Entry point               
+ *
+ *                                  
+ *
+ * Return: Always 0 (Success)       
+ *
+ */
+
+int main(void)
+{
+	int num;
+	for (num = '0'; num <= '9'; num++)
+	putchar(num);
+	if (num == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	putchar('\n');
+	return (0);
+}
