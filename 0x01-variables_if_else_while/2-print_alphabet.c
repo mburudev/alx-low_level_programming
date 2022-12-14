86a8cd5a5b2e82b3222a5da5be9df6a0efd59bf4
mburudev <ianmburu999@gmail.com>
2022-12-14 12:06:27 -0800

    Fixed for loop

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,8 +12,8 @@ int main(void)
 {
 	char alphabet;
 
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-		putchar(alphabet);
-		putchar('\n');
-		return (0);
+	for (alphabet ='a';alphabet<='z';alphabet++)
+	putchar(alphabet);
+	putchar('\n');
+	return (0);
 }
