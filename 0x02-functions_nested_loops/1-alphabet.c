79fecf44906496d0e93942d1c2424215c01eb5af
mburudev <ianmburu999@gmail.com>
2022-12-15 10:49:56 -0800

    Task one correction 7

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
- * main -Entry point
- *
- * Return: Always 0 (Success)
+ * print_alphabet - Check description
+ * Description: It prints the alphabet in lowercase followed by a new line.
+ * Return: Nothing.
  */
 
 void print_alphabet(void)
