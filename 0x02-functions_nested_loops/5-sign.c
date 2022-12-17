b453056f986b486ace7bed8f104a816113720c47
mburudev <ianmburu999@gmail.com>
2022-12-17 01:54:15 -0800

    Task 5 correction 4

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,17 +12,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-	fprintf(stderr, "+","+");
+	fprintf(stderr, "%c",'+');
 	return (1);
 	}
 	else if (n == 0)
 	{
-	fprintf(stderr, "0", "0");
+	fprintf(stderr, "%c", '0');
 	return (0);
 	}
 	else
 	{
-	fprintf(stderr, "-", "-");
+	fprintf(stderr, "%c", '-');
 	return (-1);
 	}
 }
