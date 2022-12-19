269b3cf9a6f9b92b487dfa86294e09d92912e054
mburudev <ianmburu999@gmail.com>
2022-12-19 12:24:05 -0800

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_puts - check Description
+ *Description: prints a string followed by a new line to stdout.
+ *@str: string to be output
+ *Return : none
+ */
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
