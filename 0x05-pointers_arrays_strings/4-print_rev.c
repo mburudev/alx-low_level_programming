787e162da4cf9d05294fc970b4e3fd02ef480ad9
mburudev <ianmburu999@gmail.com>
2022-12-19 13:50:34 -0800

    Task 4 correction 1

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,5 +11,14 @@
 
 void print_rev(char *s)
 {
-	return (strrev(s));
+	int len = strlen(s);
+	int i;
+	for (i = 0; i < len / 2; i++)
+	{
+	char temp = s[i];
+	s[i] = s[len - i - 1];
+	s[len - i - 1] = temp;
+	}
+	printf("%s\n", s);
+	return 0;
 }
