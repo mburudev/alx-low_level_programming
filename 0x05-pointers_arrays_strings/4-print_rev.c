f6d70c0ac496e66b722f5278c5fb9e502e785b90
mburudev <ianmburu999@gmail.com>
2022-12-19 12:31:40 -0800

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - check description
+ * Description: Prints a string in reverse.
+ * @s - the string to be printed
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	return (strrev(s));
+}
