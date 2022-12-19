944b56d92b0b487bd3bacd8fe91dc80a2203cda0
mburudev <ianmburu999@gmail.com>
2022-12-19 08:28:15 -0800

    Task 1 correction 1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,11 +5,11 @@
  * reset_to_98 - check description
  * Description : This functions takes a pointer to an int
  * updates the value to 98.
- * @n - the pointer with the address value.
+ * @n: the pointer with the address value.
  * Return: 0
  */
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
