b51b099d010cb33025fe3096290f28d2efda1bbd
mburudev <ianmburu999@gmail.com>
2022-12-19 09:45:09 -0800

    Task 2 correction 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "main.h"
+#include <string.h>
 
 /**
  * _strlen(char *s) - check description
  * Description: Returns the length of a string.
+ * strlen - returns the length of the string.
  * @s: pointer for the string
  * Return: 0
  */
 
 int _strlen(char *s)
 {
-	strlen(*s);
+	int l;
+
+	l = strlen(*s);
+	return (l);
 }
