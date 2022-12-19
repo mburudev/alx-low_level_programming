653f1b467df3d0f1ca7bbfb487ecbe1fb42a6de3
mburudev <ianmburu999@gmail.com>
2022-12-19 08:25:15 -0800

    Task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - check description
+ * Description : This functions takes a pointer to an int
+ * updates the value to 98.
+ * @n - the pointer with the address value.
+ * Return: 0
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This repo contains tasks on pointers, arrays and strings.
