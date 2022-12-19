d0eb508ac2b7408c52575f7a1062c7b0190f26d2
mburudev <ianmburu999@gmail.com>
2022-12-19 08:38:26 -0800

    Swapping values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - check description
+ * Description : It swaps the values of 2 integers
+ * @a: pointer with the address value
+ * @b: pointer with the address value
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *a = b;
+	int *b = a;
+}
