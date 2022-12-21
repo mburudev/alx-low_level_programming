6b38adccf84f637086e40b1803146d5bc3d3fdc0
mburudev <ianmburu999@gmail.com>
2022-12-20 16:09:57 -0800

    task 5 correction 1

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,9 +16,9 @@ void more_numbers(void)
 	for (i = 0; i <= 14; i++)
 	{
 	if (i > 9)
-	_putchar(i / 10 + '0');
-	_putchar(i % 10 + '0');
+	putchar(i / 10 + '0');
+	putchar(i % 10 + '0');
 	}
-	_putchar('\n');
+	putchar('\n');
 	}
 }
