91b2e4cfbaa669ba4adba3aff4c534f382460f8b
mburudev <ianmburu999@gmail.com>
2022-12-30 05:02:29 -0800

    Task 10 correction 1

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * add - Check Holberton
+ * add - Check Description
  * @a: An integer a
  * @b: An integer b
  * Description: Function that adds two integers and returns the result
