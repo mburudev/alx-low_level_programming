5e00526d40e5bc865eb054b7025ae638f82d6696
mburudev <ianmburu999@gmail.com>
2023-01-05 12:11:31 -0800

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcmp - check description.
+ * Description: it compares two strings.
+ * @s1: first input character.
+ * @s2: second input character.
+ * Return: difference in ASCII values.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, x;
+
+	for (i = 0; i != '\0'; i++)
+	{
+	if (s1[i] > s2[i])
+	{
+	x = s1[i] - s2[i];
+	return (x);
+	}
+	else if (s1[i] < s2[i])
+	{
+	x = s1[i] - s2[i];
+	return (x);
+	}
+	}
+	return (0);
+}
