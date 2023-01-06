400bd183ccf1c23f425b3b89238376ee0e43587a
mburudev <ianmburu999@gmail.com>
2023-01-06 02:14:07 -0800

    Task 3 correction 2

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,16 +11,20 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, x;
+	int i, diff;
 
-	for (i = 0; i != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; ++i)
 	{
-	if (s1[i] != s2[i])
+	if (s1[i] > s2[i])
 	{
-	x = (s1[i] - s2[i]);
-	return (x);
+	diff = s1[i] - s2[i];
+	return (diff);
 	}
-	else
+	else if (s1[i] < s2[i])
+	{
+	diff = s1[i] - s2[i];
+	return (diff);
+	}
+	}
 	return (0);
-	}
 }
