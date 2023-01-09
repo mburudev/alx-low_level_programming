097751e07a3461a16dc66c893e4e79fec6293f33
mburudev <ianmburu999@gmail.com>
2023-01-09 02:09:56 -0800

    Task 9 correction 1

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,10 +1,10 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
  * print_number - check description.
  * Description: it prints an integer
  * @n: input interger.
- * Return: resulting integer.
  */
 
 void print_number(int n)
@@ -20,6 +20,7 @@ void print_number(int n)
 	{
 	n1 = n;
 	}
+
 	if (n1 / 10)
 	print_number(n1 / 10);
 	_putchar((n1 % 10) + '0');
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,3 +10,4 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 char *leet(char *);
 char *rot13(char *);
+char _putchar(char c);
