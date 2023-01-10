e9b36b6bee2b68b23fdee5a5d1f54386310b278c
mburudev <ianmburu999@gmail.com>
2023-01-10 01:49:52 -0800

    Task 0 correction 4

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,14 +1,13 @@
-#include<stdio.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * _putchar - Check Description.
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 
-int main(void)
+int _putchar(char c)
 {
-	printf("_putchar");
-	printf("\n");
-	return (0);
+	return (write(1, &c, 1));
 }
