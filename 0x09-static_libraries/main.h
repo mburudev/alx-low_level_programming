2059e6ce1408dd512a222651fc269798aa1eaae8
mburudev <ianmburu999@gmail.com>
2023-01-10 02:20:45 -0800

    Adhered to betty coding style

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 char _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -11,3 +13,4 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+#endif /* MAIN_H */
