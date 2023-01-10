a5bf23dedd20cca7f3fc5c630a2f4bbf3accf559
mburudev <ianmburu999@gmail.com>
2023-01-10 05:44:13 -0800

    Task 1 correction 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+ 
+/**
+ * main - check description. 
+ * @argc: first parameter.
+ * @argv: second parameter
+ * Description: prints number of arguments
+ * Return: 0 for code success
+ */
 
-int main (int argc, char* argv[])
+int main(int argc, char **argv)
 {
-	printf("argc = %d/n", argc);
+	int sum;
+
+	for (sum = 0; sum < argc; sum++)
+	{
+	*argv[sum] = *argv[sum];
+	}
+	printf("%d\n", (sum - 1));
 	return (0);
 }
