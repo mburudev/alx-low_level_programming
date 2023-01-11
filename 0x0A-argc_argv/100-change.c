132887fe113e3a4e37530cf06971dfea6b8e6e48
mburudev <ianmburu999@gmail.com>
2023-01-11 03:43:31 -0800

    Task 5 correction 1

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * main - Prints the minimum number of coins to
- *        make change for an amount of money.
- * @argc: Number of command line arguments.
- * @argv: An array of the command line arguments.
- *
- * Return: If the number of arguments is not exactly one - 1.
- *         Otherwise - 0.
- */
 
+/**
+ * main - prints the minimum number of coins to
+ * make change for an amount of money
+ * @argc: number of cmd arguments
+ * @argv: array of cmd arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
 int main(int argc, char *argv[])
 {
-	int cents, coins = 0;
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
 	{
@@ -21,35 +21,24 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	cents = atoi(argv[1]);
+	num = atoi(argv[1]);
+	result = 0;
 
-	while (cents > 0)
+	if (num < 0)
 	{
-		coins++;
-		if ((cents - 25) >= 0)
-		{
-			cents -= 25;
-			continue;
-		}
-		if ((cents - 10) >= 0)
-		{
-			cents -= 10;
-			continue;
-		}
-		if ((cents - 5) >= 0)
-		{
-			cents -= 5;
-			continue;
-		}
-		if ((cents - 2) >= 0)
-		{
-			cents -= 2;
-			continue;
-		}
-		cents--;
+		printf("0\n");
+		return (0);
 	}
 
-	printf("%d\n", coins);
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
+		{
+			result++;
+			num -= coins[j];
+		}
+	}
 
+	printf("%d\n", result);
 	return (0);
-
+}
