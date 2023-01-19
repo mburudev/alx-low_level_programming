4f1cd320a94be31f9e380d6653969e97c9a67ea4
mburudev <ianmburu999@gmail.com>
2023-01-19 05:04:53 -0800

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - check description
+ * Description: locates a character in a string.
+ * @s: pointer to the string.
+ * @c: character to be matched.
+ * Return: pointer to the first occurence of or NULL if not found.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; i < strlen(s); i++)
+	{
+	if (s[i] == c)
+	{
+	return (s[i]);
+	}
+	else
+	{
+	return (NULL);
+	}
+	}
+}
