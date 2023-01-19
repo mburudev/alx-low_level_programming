453c872a71004c9e12c1d158995fecd0f04a3f0c
mburudev <ianmburu999@gmail.com>
2023-01-19 03:58:18 -0800

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memset -check description.
+ * Descrption: fills the memory with a constant byte.
+ * @s: pointer to memory block.
+ * @n: number of bytes to be set.
+ * @b: value to set memory to.
+ * Return: pointer to meory area s.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+	s[i] = b;
+	}
+	return (s);
+}
