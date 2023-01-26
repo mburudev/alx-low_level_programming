8ae521c2eff2d3bd8bc1076267532ba684c84fb8
mburudev <ianmburu999@gmail.com>
2023-01-26 04:28:21 -0800

    Task 3

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+}
+op_t;
+#endif
