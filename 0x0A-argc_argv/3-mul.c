1b70d3fa586ddb8c8e16199da5d7e0df0f4fb46b
mburudev <ianmburu999@gmail.com>
2023-04-21 23:02:16 -0700

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,16 +10,14 @@
 
 int main(int argc, char *argv[])
 {
-	int a, b, result;
+	int result;
 
 	if (argc != 3)
 	{
 	printf("Error\n");
 	return (1);
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-	result = a * b;
+	result = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", result);
 	return (0);
 }
