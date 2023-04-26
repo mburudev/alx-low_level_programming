70c2a4d3b6d39be2acf451c443dafb99046885d1
mburudev <ianmburu999@gmail.com>
2023-04-26 06:05:01 -0700

    Task 8 correction 3

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,5 +14,20 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (*s1 == '\0');
 	}
-	return (0);
+	else if (*s2 == '*')
+	{
+		while (*s1 != '\0')
+		{
+			if (wildcmp(s1, s2 + 1))
+				return (1);
+			s1++;
+		}
+		return (wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 != '\0' && (*s1 == *s2))
+		return (wildcmp(s1 + 1, s2 + 1));
+	else
+		return (0);
 }
+
+
