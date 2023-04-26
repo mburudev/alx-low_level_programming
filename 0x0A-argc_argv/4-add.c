986f6ba63a60727e47e1731954a4c9fed3ec0366
mburudev <ianmburu999@gmail.com>
2023-04-26 12:25:49 -0700

    Task 4 new corrections 2

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,17 +2,35 @@
 #include <stdlib.h>
 
 /**
- * main - entry point
- * @argc: argument count.
- * @argv: argument vector.
- * Return: 0 if no number and 1 if error.
+ * main - adds positive numbers passed as command-line arguments
+ * @argc: the number of command-line arguments
+ * @argv: an array of strings containing the command-line arguments
+ *
+ * Return: 0 if successful, 1 if an error occurred
  */
-
 int main(int argc, char *argv[])
 {
+	int sum = 0;
+
 	if (argc == 1)
 	{
-	printf("0\n");
-	return (0);
+		printf("0\n");
+		return (0);
 	}
+
+	for (int i = 1; i < argc; i++)
+	{
+		for (int j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
 }
