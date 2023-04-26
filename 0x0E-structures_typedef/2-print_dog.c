6328702e829020e3138ab1022cb88e0efb169cc8
mburudev <ianmburu999@gmail.com>
2023-04-26 10:22:31 -0700

    Task 2 correction 4

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,6 +9,8 @@
  */
 void print_dog(struct dog *d)
 {
+	if (d)
+	{
 	if (d == NULL)
 		return;
 
@@ -23,4 +25,5 @@ void print_dog(struct dog *d)
 		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
+	}
 }
