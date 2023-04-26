34841127a934c4250272eeed60aeee3b336e7658
mburudev <ianmburu999@gmail.com>
2023-04-26 02:16:17 -0700

    Task 7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * is_palindrome - checks of a string is a palindrome.
+ * @s: pointer to the string.
+ * Return: 1 if string is a palindrome and 0 if not.
+ */
+
+int is_palindrome(char *s)
+{
+	if (s[0] == '\0')
+	{
+		return (0);
+	}
+	return (1);
+}
