5b7de1b68f70d0d14ec03534f0808cc34680dabf
mburudev <ianmburu999@gmail.com>
2023-04-26 11:43:21 -0700

    Task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * free_dog - frees dogs.
+ * @d: pointer to struct dog.
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d == NULL)
+		{
+			return;
+		}
+	}
