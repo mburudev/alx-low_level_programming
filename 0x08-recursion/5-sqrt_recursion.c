7e29b1ebba597f72e79f5a20e59470a1a620f6af
mburudev <ianmburu999@gmail.com>
2023-04-26 00:31:47 -0700

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,14 +9,26 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n < 1)
+	if (n < 0)
 	{
 		return (-1);
 	}
-	if (n == 1)
+	else if (n == 0 || n == 1)
 	{
-		return (1);
+		return (n);
 	}
+	else
+	{
+		int i;
 
-	return (_sqrt_recursion((n + 1) * (n + 1)));
+		for (i = 1; i * i < n; i++)
+		if (i * i == n)
+		{
+			return (i);
+		}
+		else
+		{
+		return (-1);
+		}
+	}
 }
