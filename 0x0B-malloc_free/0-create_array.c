2acea027711665da976083fa5a52dee3cf847797
mburudev <ianmburu999@gmail.com>
2023-04-26 03:18:38 -0700

    Task 0 correction 2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,13 +10,14 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *array;
 	unsigned int i;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	char *array = (char *) malloc(size * sizeof(char));
+	array = malloc(size * sizeof(char));
 
 	if (array == NULL)
 	{
