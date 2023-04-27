982292d528cb067d54b26e7e6d88630f80134f84
mburudev <ianmburu999@gmail.com>
2023-04-27 06:21:13 -0700

    Task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * my_print - prints a string before the main function executes.
+ */
+
+void __attribute__((constructor)) my_print(void);
+
+void my_print(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
