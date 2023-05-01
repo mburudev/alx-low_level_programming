609dbda645854323f7f7758ffa99e5aa645cbdf0
mburudev <ianmburu999@gmail.com>
2023-05-01 02:54:20 -0700

    Task 3 added header file

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,16 +1,19 @@
-#ifndef CALC_H
-#define CALC_H
+#ifndef _3_CALC_H_
+#define _3_CALC_H_
+
+#include <stdlib.h>
+
 /**
  * struct op - Struct op
- *
  * @op: The operator
  * @f: The function associated
  */
-
 typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
-}
-op_t;
-#endif
+} op_t;
+
+int (*get_op_func(char *s))(int, int);
+
+#endif /* _3_CALC_H_ */
