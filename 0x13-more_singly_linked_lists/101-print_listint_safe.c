d261c4d87f0a360ef65d6ca940c80a175e43f5f8
mburudev <ianmburu999@gmail.com>
2023-05-02 08:14:53 -0700

    Task 12 correction 2

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,33 +1,42 @@
+#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "lists.h"
 
 /**
- * print_listint_safe - prints a listint_t linked list
- * @head: pointer to the head of the list
- * Return: the number of nodes in the list
+ * print_listint_safe - Prints a listint_t linked list
+ * @head: Pointer to the head of the list
+ *
+ * Return: The number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
     size_t count = 0;
-    const listint_t *current = head, *loop_node = NULL;
-    
-    while (current != NULL)
+    const listint_t *slow = head, *fast = head;
+
+    while (slow != NULL && fast != NULL && fast->next != NULL)
     {
-        printf("[%p] %d\n", (void *)current, current->n);
+        printf("[%p] %d\n", (void *)slow, slow->n);
+        slow = slow->next;
+        fast = fast->next->next;
         count++;
-        current = current->next;
-        if (current == loop_node)
+
+        if (slow == fast)
         {
-            printf("-> [%p] %d\n", (void *)current, current->n);
+            printf("[%p] %d\n", (void *)slow, slow->n);
+            printf("-> [%p] %d\n", (void *)fast->next, fast->next->n);
             printf("-> Loop detected, exit\n");
             exit(98);
         }
-        if (current != NULL && current < loop_node)
-        {
-            loop_node = current;
-        }
     }
-    
-    return count;
+
+    while (head != slow)
+    {
+        printf("[%p] %d\n", (void *)head, head->n);
+        head = head->next;
+        count++;
+    }
+
+    printf("[%p] %d\n", (void *)head, head->n);
+    return (count + 1);
 }
+
