ec8c832fb7aec14c400b789f1b22de0248933a83
mburudev <ianmburu999@gmail.com>
2023-05-03 02:45:24 -0700

    Task 6 correction 2

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,9 @@
-#include <stdio.h>
-
-/**
- * main -  entry point
- * Return: 0
- */
-
-int main()
-{
-	printf("Hello, Holberton\n");
-	return 0;
-}
+          global    main
+          extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
