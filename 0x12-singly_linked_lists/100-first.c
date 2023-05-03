80a0ee2e3332f4ea6329bb67bb21d30887ff4d03
mburudev <ianmburu999@gmail.com>
2023-05-03 02:53:09 -0700

    Task 5 betty 2

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
+void my_print(void) __attribute__((constructor));
+
 /**
  * my_print - prints a string before the main function executes.
  */
 
-void __attribute__((constructor)) my_print(void);
-
-void my_print()
+void my_print(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
