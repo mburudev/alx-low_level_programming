f24e19e888f75e187ff5c239270799422862df0e
mburudev <ianmburu999@gmail.com>
2023-05-03 01:48:46 -0700

    Task 7 correction 5

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,21 +8,39 @@
  * Return: 1 if string is a palindrome and 0 if not.
  */
 
-int is_palindrome(char *s)
+int _strlen_recursion(char *s)
 {
-	int len = 0, i = 0;
-
-	/* Compute the length of the string */
-	while (s[len] != '\0')
-		len++;
-
-	/* Check if the string is a palindrome */
-	while (i < len / 2)
-	{
-		if (s[i] != s[len - i - 1])
-			return 0;
-		i++;
-	}
-	return 1;
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
 
+/**
+ * check_palindrome - checks if a string is a palindrome
+ * @s: pointer to the string to check
+ * @len: length of the string
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise
+ */
+int check_palindrome(char *s, int len)
+{
+	if (len <= 1)
+		return (1);
+	if (*s == *(s + len - 1))
+		return (check_palindrome(s + 1, len - 2));
+	return (0);
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: pointer to the string to check
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	int len;
+
+	len = _strlen_recursion(s);
+	return (check_palindrome(s, len));
+}
