fbba38fd6dafda4a34595df7429767b33813833f
mburudev <ianmburu999@gmail.com>
2023-05-03 00:43:34 -0700

    Task 4 correction 2

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,44 +11,43 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	unsigned int i;
-    void *new_ptr = NULL;
-    
-    if (ptr == NULL)
-    {
-        new_ptr = malloc(new_size);
-        if (new_ptr == NULL)
-        {
-            return NULL;
-        }
-        else
-        {
-            return new_ptr;
-        }
-    }
-    else if (new_size == 0)
-    {
-        free(ptr);
-        return NULL;
-    }
-    else if (new_size <= old_size)
-    {
-        return ptr;
-    }
-    else
-    {
-        new_ptr = malloc(new_size);
-        if (new_ptr == NULL)
-        {
-            return NULL;
-        }
-        else
-        {
-            for (i = 0; i < old_size; i++)
-            {
-                *((char *)new_ptr + i) = *((char *)ptr + i);
-            }
-            free(ptr);
-            return new_ptr;
-        }
-    }
+	void *new_ptr = NULL;
+
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			return (new_ptr);
+		}
+	} else if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else if (new_size <= old_size)
+	{
+		return (ptr);
+	}
+	else
+	{
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			for (i = 0; i < old_size; i++)
+			{
+				*((char *)new_ptr + i) = *((char *)ptr + i);
+			}
+			free(ptr);
+			return (new_ptr);
+		}
+	}
 }
