c6ff31ccd6d560edadf0a9f1f82bf451a0cb095a
mburudev <ianmburu999@gmail.com>
2023-05-03 01:44:25 -0700

    Task 7 correction 4

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,14 +10,19 @@
 
 int is_palindrome(char *s)
 {
-	int len = 0, i;
+	int len = 0, i = 0;
 
+	/* Compute the length of the string */
 	while (s[len] != '\0')
 		len++;
-	for (i = 0; i < len / 2; i++)
+
+	/* Check if the string is a palindrome */
+	while (i < len / 2)
 	{
 		if (s[i] != s[len - i - 1])
-			return (0);
+			return 0;
+		i++;
 	}
-	return (1);
+	return 1;
 }
+
