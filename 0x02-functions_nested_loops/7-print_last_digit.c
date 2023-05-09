1ffd37221c8646f0c7ccf494cbf46b79653f117d
mburudev <ianmburu999@gmail.com>
2023-05-09 07:39:57 -0700

    Task 7 correction 1

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_last_digit - prints the last digit of a number 
+ * print_last_digit - prints the last digit of a number
  * @r: An integer input
  * Return: last digit of number r
  */
