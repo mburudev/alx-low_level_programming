51c47ccac929f8a8e8a95017776992771e26cdc7
mburudev <ianmburu999@gmail.com>
2023-05-22 09:40:43 -0700

    Task 13

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include "main.h"
+#include <string.h>
 
 /**
- * *infinite_add - Check description.
- * Description: It adds two numbers.
+ * *infinite_add - adds two numbers.
  * @n1: input number.
  * @n2: input number.
  * @r: the storage buffer.
  * @size_r: the buffer size.
  * Return: A pointer to the result.
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
+	int i;
 	int l1 = strlen(n1);
 	int l2 = strlen(n2);
 	int maxlen = (l1 > l2) ? l1 : l2;
@@ -21,7 +21,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (maxlen + 1 > size_r)
 	return (0);
 	memset(r, '\0', size_r);
-	for (int i = 0; i < maxlen; i++)
+	for (i = 0; i < maxlen; i++)
 	{
 	int d1 = (i < l1) ? n1[l1 - i - 1] - '0' : 0;
 	int d2 = (i < l2) ? n2[l2 - i - 1] - '0' : 0;
