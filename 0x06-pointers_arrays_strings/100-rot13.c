31fd8343836237c05312ca44fc877fa2e26ef812
mburudev <ianmburu999@gmail.com>
2023-05-22 10:16:50 -0700

    Task 8 correction 1

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * rot13 - Encode a string using ROT13
  * @str: Pointer to the string
+ *
  * Return: Pointer to the resulting string
  */
 char *rot13(char *str)
@@ -13,26 +13,47 @@ char *rot13(char *str)
 	char *alpha_lower = "abcdefghijklmnopqrstuvwxyz";
 	char *rot13_upper = "NOPQRSTUVWXYZABCDEFGHIJKLM";
 	char *rot13_lower = "nopqrstuvwxyzabcdefghijklm";
-	int i;
+	int i, j;
 
 	while (*ptr != '\0')
 	{
 		i = 0;
+		j = 0;
+
+		/* Check if the character is an uppercase letter */
 		while (alpha_upper[i] != '\0')
 		{
 			if (*ptr == alpha_upper[i])
 			{
 				*ptr = rot13_upper[i];
+				j = 1;  /* Indicate that an uppercase letter was found */
 				break;
 			}
-			else if (*ptr == alpha_lower[i])
+			i++;
+		}
+
+		/* If an uppercase letter was found, continue to the next character */
+		if (j)
+		{
+			ptr++;
+			continue;
+		}
+
+		i = 0;
+
+		/* Check if the character is a lowercase letter */
+		while (alpha_lower[i] != '\0')
+		{
+			if (*ptr == alpha_lower[i])
 			{
 				*ptr = rot13_lower[i];
 				break;
 			}
 			i++;
 		}
+
 		ptr++;
 	}
-	return (str);
+
+	return str;
 }
