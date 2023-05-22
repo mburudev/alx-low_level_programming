35afb1dd269ae960c05a77ca2b4834ff5cd35dd7
mburudev <ianmburu999@gmail.com>
2023-05-22 09:12:19 -0700

    Task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,29 +2,37 @@
 #include "main.h"
 
 /**
- * *rot13 -check description.
- * Description: Encodes astring using rot13.
- * @s: string to be encoded.
- * Return: Resulting encoded string.
+ * rot13 - Encode a string using ROT13
+ * @str: Pointer to the string
+ * Return: Pointer to the resulting string
  */
-
-char *rot13(char *s)
+char *rot13(char *str)
 {
-	int i, j;
+	char *ptr = str;
+	char *alpha_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *alpha_lower = "abcdefghijklmnopqrstuvwxyz";
+	char *rot13_upper = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *rot13_lower = "nopqrstuvwxyzabcdefghijklm";
+	int i;
 
-	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-	for (i = 0; s[i] != '\0'; i++)
+	while (*ptr != '\0')
 	{
-	for (j = 0; a[j] != '\0'; j++)
-	{
-	if (s[i] == a[j])
-	{
-	s[i] = b[j];
-	break;
+		i = 0;
+		while (alpha_upper[i] != '\0')
+		{
+			if (*ptr == alpha_upper[i])
+			{
+				*ptr = rot13_upper[i];
+				break;
+			}
+			else if (*ptr == alpha_lower[i])
+			{
+				*ptr = rot13_lower[i];
+				break;
+			}
+			i++;
+		}
+		ptr++;
 	}
-	}
-	}
-	return (s);
+	return (str);
 }
