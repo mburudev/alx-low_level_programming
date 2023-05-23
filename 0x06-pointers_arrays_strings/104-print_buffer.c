509a5bda82fc6209a27c7aeda99ab0e3ff2b1cdb
mburudev <ianmburu99@gmail.com>
2023-05-23 04:00:04 -0700

    adhered to betty

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -3,32 +3,36 @@
 #include <ctype.h>
 
 /**
- * infinite_add - Adds two numbers
- * @n1: The first number
- * @n2: The second number
- * @r: The buffer that will store the result
- * @size_r: The size of the buffer
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
  *
- * Return: A pointer to the result, or 0 if the result cannot be stored in the buffer
+ * Return: void
  */
-void print_buffer(char *b, int size) {
+void print_buffer(char *b, int size)
+{
 	int i, j;
-  if (size <= 0) {
-    printf("\n");
-    return;
-  }
 
-  for (i = 0; i < size; i += 10) {
-    printf("%08x ", i);
-    for ( j = i; j < i + 10 && j < size; j++) {
-      printf("%02x ", b[j]);
-      if (isprint(b[j])) {
-        printf("%c", b[j]);
-      } else {
-        printf(".");
-      }
-    }
-    printf("\n");
-  }
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x ", i);
+		for (j = i; j < i + 10 && j < size; j++)
+		{
+			printf("%02x ", b[j]);
+			if (isprint(b[j]))
+			{
+				printf("%c", b[j]);
+			}
+			else
+			{
+				printf(".");
+			}
+		}
+		printf("\n");
+	}
 }
-
