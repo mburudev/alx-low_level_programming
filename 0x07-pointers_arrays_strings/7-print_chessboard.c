545a30a00392eea5bca1ecd641fcc72b14a7c934
mburudev <ianmburu99@gmail.com>
2023-05-23 05:24:12 -0700

    Actual Task 7 corrections

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * print_chessboard - Prints the chessboard
@@ -8,15 +9,12 @@ void print_chessboard(char (*a)[8])
 {
 	int row, col;
 
-	if (a == NULL)
-		return;
-
 	for (row = 0; row < 8; row++)
 	{
 		for (col = 0; col < 8; col++)
 		{
-			printf("%c ", a[row][col]);
+			putchar(a[row][col]);
 		}
-		printf("\n");
+		putchar('\n');
 	}
 }
