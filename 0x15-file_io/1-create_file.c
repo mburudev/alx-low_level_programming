ceef468323f2e780ac8fc65b5212f1c0fe833e10
mburudev <ianmburu99@gmail.com>
2023-05-23 09:42:18 -0700

    Task 1 correction 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,27 +5,36 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+/**
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor;
-	
-    if (filename == NULL)
-        return -1;
+	int file_desc;
 
-    file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (file_descriptor == -1)
-        return -1;
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        ssize_t bytes_written = write(file_descriptor, text_content, strlen(text_content));
-        if (bytes_written == -1)
-        {
-            close(file_descriptor);
-            return -1;
-        }
-    }
+	file_desc = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
-    close(file_descriptor);
-    return 1;
+	if (file_desc == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		ssize_t bytes_written = write(file_desc, text_content, strlen(text_content));
+
+		if (bytes_written == -1)
+		{
+			close(file_desc);
+			return (-1);
+		}
+	}
+	close(file_desc);
+	return (1);
 }
