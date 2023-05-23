6069d9c1b25ce8d278581bf095e3153146b31f17
mburudev <ianmburu99@gmail.com>
2023-05-23 04:46:38 -0700

    Task 3 corrections

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
-#include "main.h"
+#include <string.h>
 
 /**
- *
+ * *_strspn - gets the length of a prefix substring
+ * @s: string to evaluate
+ * @accept: string containing the list of characters to match in s
+ * Return: the number of bytes in the initial s
+ * segment which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+
+	while (s[i] != '\0' && strchr(accept, s[i]) != NULL)
+	{
+		i++;
+	}
+	return (i);
+}
