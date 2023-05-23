2ea3a3249bcb7e8288dbaf72a844837945ca3594
mburudev <ianmburu99@gmail.com>
2023-05-23 03:37:02 -0700

    Task 12 corrections

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,35 +1,38 @@
 #include "main.h"
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
-void print_buffer(char *b, int size)
-{
-	int i;
-	int j;
+/**
+ * infinite_add - Adds two numbers
+ * @n1: The first number
+ * @n2: The second number
+ * @r: The buffer that will store the result
+ * @size_r: The size of the buffer
+ *
+ * Return: A pointer to the result, or 0 if the result cannot be stored in the buffer
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r) {
+  int i, j, carry = 0;
+  int len1 = strlen(n1);
+  int len2 = strlen(n2);
 
-    if (size <= 0) {
-        printf("\n");
-        return;
-    }
+  
+  if (len1 + len2 > size_r) {
+    return 0;
+  }
 
-    for (i = 0; i < size; i += 10) {
-      
-        printf("%08x  ", i);
+  
+  for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0; i--, j--) {
+    int sum = (i >= 0 ? n1[i] - '0' : 0) + (j >= 0 ? n2[j] - '0' : 0) + carry;
+    r[i + j + 1] = sum % 10 + '0';
+    carry = sum / 10;
+  }
 
-     
-        for (j = i; j < i + 10; j++) {
-            if (j < size) {
-                unsigned char byte = (unsigned char)b[j];
-                printf("%02x ", byte);
-                if (isprint(byte))
-                    printf("%c", byte);
-                else
-                    printf(".");
-            } else {
-                printf("   "); 
-            }
-        }
+  
+  if (carry) {
+    r[0] = carry + '0';
+  }
 
-        printf("\n");
-    }
+  return r;
 }
