e3bbcc6b2430016290b63259c9739e76c00f20bd
mburudev <ianmburu999@gmail.com>
2023-06-24 00:37:43 -0700

    Task 3 correction 2

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp gm gm_modified
-chmod +x gm_modified
+wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
