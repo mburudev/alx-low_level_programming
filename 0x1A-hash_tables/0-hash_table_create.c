27e4e08015e3858033989774866dc82b916f4c6d
mburudev <ianmburu999@gmail.com>
2023-07-14 03:05:31 -0700

    Task 0 correction 1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,60 +1,27 @@
 #include <stdlib.h>
-
-/**
- * struct hash_node_s - Node of a hash table
- *
- * @key: The key, string
- *        The key is unique in the HashTable
- * @value: The value corresponding to a key
- * @next: A pointer to the next node of the List
- */
-typedef struct hash_node_s
-{
-    char *key;
-    char *value;
-    struct hash_node_s *next;
-} hash_node_t;
-
-/**
- * struct hash_table_s - Hash table data structure
- *
- * @size: The size of the array
- * @array: An array of size @size
- *         Each cell of this array is a pointer to the first node of a linked list,
- *         because we want our HashTable to use a Chaining collision handling
- */
-typedef struct hash_table_s
-{
-    unsigned long int size;
-    hash_node_t **array;
-} hash_table_t;
+#include "hash_tables.h"
 
 /**
  * hash_table_create - Create a hash table
  * @size: The size of the array
  *
- * Return: A pointer to the newly created hash table, or NULL if something went wrong
+ * Return: Pointer to newly created hash table / NULL if something went wrong
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *table = malloc(sizeof(hash_table_t));
-    unsigned long int i;
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+	unsigned long int i;
 
-    if (table == NULL)
-        return NULL;  /* Failed to allocate memory for the hash table */
-
-    table->size = size;
-    table->array = malloc(sizeof(hash_node_t *) * size);
-
-    if (table->array == NULL)
-    {
-        free(table);  /* Free the previously allocated memory for the hash table */
-        return NULL;  /* Failed to allocate memory for the array */
-    }
-
-    /* Initialize all array elements to NULL */
-    for (i = 0; i < size; i++)
-        table->array[i] = NULL;
-
-    return table;
+	if (table == NULL)
+		return (NULL);
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size)
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+	return (table);
 }
