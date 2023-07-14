c7a20f170ab37b3777f063e12300b273fb1ccab4
mburudev <ianmburu999@gmail.com>
2023-07-14 05:39:15 -0700

    Task 3 adhered to betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,17 +11,15 @@
  */
 hash_node_t *create_new_node(const char *key, const char *value)
 {
-    hash_node_t *new_node;
+	hash_node_t *new_node;
 
-    new_node = malloc(sizeof(hash_node_t));
-    if (new_node == NULL)
-        return (NULL);
-
-    new_node->key = strdup(key);
-    new_node->value = strdup(value);
-    new_node->next = NULL;
-
-    return (new_node);
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = NULL;
+	return (new_node);
 }
 
 /**
@@ -34,44 +32,38 @@ hash_node_t *create_new_node(const char *key, const char *value)
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    hash_node_t *node;
-    hash_node_t *new_node;
-    unsigned long int index;
+	hash_node_t *node;
+	hash_node_t *new_node;
+	unsigned long int index;
 
-    if (ht == NULL || key == NULL || *key == '\0')
-        return (0);
-
-    index = key_index((const unsigned char *)key, ht->size);
-    node = ht->array[index];
-
-    if (node == NULL)
-    {
-        new_node = create_new_node(key, value);
-        if (new_node == NULL)
-            return (0);
-
-        ht->array[index] = new_node;
-        return (1);
-    }
-
-    /* If key exists, replace value */
-    while (node != NULL)
-    {
-        if (strcmp(key, node->key) == 0)
-        {
-            free(node->value);
-            node->value = strdup(value);
-            return (1);
-        }
-        node = node->next;
-    }
-
-    /* If key doesn't exist, create new node */
-    new_node = create_new_node(key, value);
-    if (new_node == NULL)
-        return (0);
-
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
-    return (1);
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+	if (node == NULL)
+	{
+		new_node = create_new_node(key, value);
+		if (new_node == NULL)
+			return (0);
+		ht->array[index] = new_node;
+		return (1);
+	}
+	/* If key exists, replace value */
+	while (node != NULL)
+	{
+		if (strcmp(key, node->key) == 0)
+		{
+			free(node->value);
+			node->value = strdup(value);
+			return (1);
+		}
+		node = node->next;
+	}
+	/* If key doesn't exist, create new node */
+	new_node = create_new_node(key, value);
+	if (new_node == NULL)
+		return (0);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
 }
