371fc05b0a27390cdbfed8a97614fcfba4313b36
mburudev <ianmburu999@gmail.com>
2023-07-14 06:04:25 -0700

    Task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,15 +1,30 @@
 #include "hash_tables.h"
 
-void hash_table_print(const hash_table_t* ht) {
-    if (ht == NULL)
-        return;
+/**
+ * hash_table_print - prints the hashtable
+ * @ht: the hash table you want to print
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+	short int comma = 0;
 
-    for (unsigned int i = 0; i < HASH_TABLE_SIZE; i++) {
-        hash_table_node_t* current = ht->buckets[i];
-
-        while (current != NULL) {
-            printf("Key: %s, Value: %s\n", current->key, current->value);
-            current = current->next;
-        }
-    }
+	if (ht == NULL)
+		return;
+	putchar('{');
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (comma)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			node = node->next;
+			if (comma == 0)
+				comma = 1;
+		}
+	}
+	printf("}\n");
 }
